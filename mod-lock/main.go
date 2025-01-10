df8ac27a98a58e22d94879a5390b1101b2e07ae1
Marlom Souza <marlom1012@gmail.com>
2025-01-09 20:31:19 -0400

    channel buffer

--- mod-lock/main.go
+++ mod-lock/main.go
@@ -3,24 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	channel := make(chan int)
+	channel := make(chan int, 100) // BUFFER
 	go setList(channel)
-	go setList2()
+	
 	for v := range channel {
-		fmt.Println(v)
+		fmt.Println("reading", v)
 	}
 }
+// setList(channel <-chan int)  <- only read
+// setList(channel chan<- int)  <- insert
+func setList(channel chan<- int) {
 
-func setList(channel chan int) {
 	for i := 0; i < 100; i++ {
 		channel <- i
+		fmt.Println("insert", i)
 	}
 	close(channel)
 }
 
-func setList2() {
-	for i := 100; i >=0 ; i-- {
-		fmt.Println("Set list 2", i)
-	}
-	
-}
