93aad3d7745f5cae5a2af108d6bb996c646ac210
Marlom Souza <marlom1012@gmail.com>
2025-01-10 14:27:39 -0400

    test: add unit test for HandlerError to verify successful response handling

--- emailN/internal/endpoints/handler_error_test.go
+++ emailN/internal/endpoints/handler_error_test.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	internalerrors "emailn/internal/internal-errors"
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
@@ -38,5 +39,25 @@ func Test_HandlerError_when_endpoint_returns_domain_error(t *testing.T) {
 
 	assert.Equal(http.StatusBadRequest, res.Code)
 	assert.Contains(res.Body.String(), "domain error")
+}
 
+func Test_HandlerError_when_endpoint_returns_ok(t *testing.T) {
+	assert := assert.New(t)
+	type bodyForTest struct {
+		Id int
+	}
+	expectedObj := bodyForTest{Id: 2}
+	endpointsWithDomainError := func(w http.ResponseWriter, r *http.Request) (EndpointStruct, error) {
+		return EndpointStruct{Obj: expectedObj, Status: http.StatusOK}, nil
+	}
+	req, _ := http.NewRequest(http.MethodGet, "/", nil)
+	res := httptest.NewRecorder()
+	sut := HandlerError(endpointsWithDomainError)
+
+	sut.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	result := bodyForTest{}
+	json.Unmarshal(res.Body.Bytes(), &result)
+	assert.Equal(expectedObj, result)
 }
